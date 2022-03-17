6fef2c3d764662fa257d51ff6ee3ce17009dfa3c
Michael Bouclas <mbouclas@gmail.com>
2022-03-17 21:40:20 +0200

    fix on build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
-import dotenv from 'dotenv'
-dotenv.config();
+import dotenv from 'dotenv';
+import fs from 'fs';
+let path;
+if (fs.existsSync('/etc/secrets/.env')) {
+	path = '/etc/secrets/.env';
+}
+
+dotenv.config({path});
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
